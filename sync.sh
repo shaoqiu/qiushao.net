9e3c58c688e69062494e59e02daa5d7342abb27e
shaoqiu <360325900@qq.com>
2015-07-07 16:17:02 +0800

    修正sync.sh排除目录方法

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync --delete -avzP ./ shaoqiu@45.33.7.41:/var/www/qiushao.net/public_html --exclude=".git .idea __pycache__"
+rsync --delete -avzP ./ shaoqiu@45.33.7.41:/var/www/qiushao.net/public_html --exclude=.git --exclude=.idea --exclude=__pycache__
